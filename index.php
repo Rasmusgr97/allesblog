462da90a99439b942aee0fd338af1bfdd15b6349
Jonas Røssum <jonas@bitspace.dk>
2013-12-26 01:32:28 +0100

    merged menu.html into index

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Allesblog</title>
+</head>
+<body>
+	<div id='cssmenu'>
+	<ul>
+	   <li class='active'><a href='index.html'><span>Dagens indlæg</span></a></li>
+	   <li><a href='tidligere.html'><span>Tidligere  indlæg</span></a></li>
+	   <li><a href='information.html'><span>Information</span></a></li>
+	   <li class='last'><a href='skriv.html'><span>Skriv til os</span></a></li>
+	</ul>
+	</div>
+</body>
+</html>
